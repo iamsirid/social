6d5c5932a9e1eb7b1a4d3c6431f85d7f1020b007
iamsirid <iamsirid@gmail.com>
2024-11-25 10:09:01 +0700

    add followers

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -22,10 +22,17 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 
 }
 
+func (app *application) conflictResponse(w http.ResponseWriter, r *http.Request, err error) {
+
+	log.Printf("conflict error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+
+	writeJSONError(w, http.StatusConflict, err.Error())
+
+}
+
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
 
 	log.Printf("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err)
 
 	writeJSONError(w, http.StatusNotFound, "resource not found")
-
 }
